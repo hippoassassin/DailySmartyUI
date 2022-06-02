9acda5ce5e26ae96fb5d9df2af7218155a86e5ba
hippoassassin <jeremiahevansbro@gmail.com>
2022-06-01 21:06:53 -0500

    setup logo component and logo styls

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import Logo from "./logo";
 
 export default class App extends Component {
   render() {
-    return <div>Logo</div>;
+    return (
+      <div>
+        <Logo />
+      </div>
+    );
   }
 }
---
+++ src/components/logo.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Logo extends Component {
+  render() {
+    return (
+      <div className="logo-main">
+        <img
+          alt="Daily smarty ui image logo"
+          src="/assets/ds_circle_logo.png"
+        />
+      </div>
+    );
+  }
+}
+
+export default Logo;
