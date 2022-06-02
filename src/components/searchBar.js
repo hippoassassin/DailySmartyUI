c86277e3cedf687b464c195c46242f1e445bcaaa
hippoassassin <jeremiahevansbro@gmail.com>
2022-06-01 21:22:12 -0500

    created posts and searchbar components

---
+++ src/components/searchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Logo extends Component {
+  render() {
+    return (
+      <form className="logo-main">
+        <img />
+      </form>
+    );
+  }
+}
+
+export default Logo;
