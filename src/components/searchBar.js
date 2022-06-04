f630dda7b3416f64bc366288a8db648007925d61
hippoassassin <jeremiahevansbro@gmail.com>
2022-06-03 20:49:48 -0500

    routed handle form submit

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
+import { withRouter } from "react-router-dom";
+
 class SearchBar extends Component {
-  handleFormSubmit = function ({ query }) {};
+  handleFormSubmit = function ({ query }) {
+    console.log("trying to handle submit for query", query);
+    this.props.history.push("/results");
+  };
   renderInput(field) {
     return (
       <input type="text" placeholder="Search DailySmarty" {...field.input} />
@@ -25,4 +30,6 @@ class SearchBar extends Component {
 SearchBar = reduxForm({
   form: "searchBar",
 })(SearchBar);
+
+SearchBar = withRouter(SearchBar);
 export default SearchBar;
