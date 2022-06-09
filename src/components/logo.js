dc0793c0f1ea2662a9f7f015734d8cde49882932
hippoassassin <jeremiahevansbro@gmail.com>
2022-06-08 19:54:00 -0500

    modified logo component to take custom sz

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,9 +2,14 @@ import React, { Component } from "react";
 
 class Logo extends Component {
   render() {
+    const size = {
+      height: this.props.size ? this.props.size : 105,
+      width: this.props.size ? this.props.size : 105,
+    };
     return (
       <div className="logo-main">
         <img
+          style={size}
           alt="Daily smarty ui image logo"
           src="/assets/ds_circle_logo.png"
         />
