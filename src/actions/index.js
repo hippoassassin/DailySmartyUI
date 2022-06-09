00c0fe45c53e6f828bc40b65db7d31da6269a291
hippoassassin <jeremiahevansbro@gmail.com>
2022-06-08 19:29:47 -0500

    added post topics to post components

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import { SET_RECENT_POSTS } from "./types";
 import axios from "axios";
 
+
 export function fetchRecentPosts() {
   return function (dispatch) {
     axios.get("https://api.dailysmarty.com/posts").then((response) => {
